e77840d6227be4edcfe35df7ac65640c0be8f791
NickBryan26 <nblopezr@pucese.edu.ec>
2021-07-21 23:04:50 -0500

    deber en clases

---
+++ cmake-build-debug/CMakeFiles/TRABAJO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TRABAJO.dir/main.cpp.obj"
+  "TRABAJO.exe"
+  "TRABAJO.exe.manifest"
+  "TRABAJO.pdb"
+  "libTRABAJO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TRABAJO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
